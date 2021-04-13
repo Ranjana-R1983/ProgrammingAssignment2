b801b1f25e4c3743dad643db251f1082f3c83247
Ranjana-R1983 <82327760+Ranjana-R1983@users.noreply.github.com>
2021-04-13 13:14:00 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment : Caching the Inverse of a Matrix
+## Course Era Registrant : Ranjana R
 
-## Write a short comment describing this function
+## Functions to calculate the inverse of a inversible matrix with non zero determinant and store in cache with write and read modes
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+    invmatrix <- NULL
+	set <- function(y) 
+	{
+          x <<- y
+          invmatrix <<- NULL
+	}
+  
+	get <- function() x
+	setinverse <- function(inverse) invmatrix <<- inverse
+	getinverse <- function() invmatrix
+	list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Check for cached data for the available inverse value, if not compute using solve() function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+        ## Function to check cached data, if round return the value, else compute using solve() function
+	invmatrix <- x$getinverse()
+	if (!is.null(invmatrix)) 
+	{
+          message("getting cached data")
+          return(invmatrix)
+	}
+	data <- x$get()
+	invmatrix <- solve(data, ...)
+	x$setinverse(invmatrix)
+	invmatrix
 }
